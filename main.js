9c59d50034d7407a0d0b577bf696a72a9bea7310
cosimogiove <cosimo18497@gmail.com>
2023-12-05 15:45:48 +0100

    feat: esercizio finito senza css

--- main.js
+++ main.js
@@ -2,9 +2,23 @@
 const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
+    data() {
+        return {
+            emailList: [],
+            quantita : 10
+        }
+    },
+    created() {
+        this.emailList1()
+    },
+    methods:{
+      emailList1(){
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((email) => {
+                    this.emailList.push(email.data.response)
+                });
+        }
+      }
     }
-  },
 }).mount('#app')
